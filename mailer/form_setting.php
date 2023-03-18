04582f2c98c3815a134a8885bc628480e525bd23
Elizabeth Rodricks <elizabethrodricks@Elizabeths-iMac.local>
2023-03-18 16:23:34 -0400

    stuff

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site Relense"; //letter subject
+	$to = 'rodricksnyc@gmail.com'; // Enter Your E-mail
+	$from = 'ceo@relense.com'; // Admin e-mail
+	$fromName = 'Relense'; // Your company name
+	$charset = 'UTF-8';
+?>
