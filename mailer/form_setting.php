370de260499d0ab4d7909e801dcccd5fd90f627f
Elizabeth Rodricks <elizabethrodricks@Elizabeths-iMac.local>
2023-03-20 13:59:41 -0400

    ->SMTPSecure = 'ssl';

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from Relense"; //letter subject
-	$to = 'ceo@relense.com'; // Enter Your E-mail
-	$from = 'ceo@relense.com'; // Admin e-mail
-	$fromName = 'Relense'; // Your company name
-	$charset = 'UTF-8';
-?>
